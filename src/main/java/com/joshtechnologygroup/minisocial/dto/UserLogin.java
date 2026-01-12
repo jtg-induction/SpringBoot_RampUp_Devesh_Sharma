dbacfa3797cd500f019c1347a50cdf68bd6eda3a
Devesh <devesh.sharma@joshtechnologygroup.com>
2026-01-12 11:15:23 +0530

    DS_RU_02: Merge with DS_RU_01

--- src/main/java/com/joshtechnologygroup/minisocial/dto/UserLogin.java
+++ src/main/java/com/joshtechnologygroup/minisocial/dto/UserLogin.java
@@ -1,18 +1,6 @@
 package com.joshtechnologygroup.minisocial.dto;
 
-<<<<<<< HEAD
 public record UserLogin(
         String email,
         String password
-) {
-=======
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UserLogin {
-    private String email;
-    private String password;
->>>>>>> 0b4eb02 (feat: Authentication)
-}
+) {}
