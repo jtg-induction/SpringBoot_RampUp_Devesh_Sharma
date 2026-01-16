d133f817f6363f6af3c0b6948208cd8cc09ce95a
Devesh <devesh.sharma@joshtechnologygroup.com>
2026-01-16 13:51:23 +0530

    DS_RU_03: Regex for strong password check

--- src/main/java/com/joshtechnologygroup/minisocial/dto/UpdatePasswordRequest.java
+++ src/main/java/com/joshtechnologygroup/minisocial/dto/UpdatePasswordRequest.java
@@ -2,10 +2,12 @@ package com.joshtechnologygroup.minisocial.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.joshtechnologygroup.minisocial.util.Validations;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+
+import static com.joshtechnologygroup.minisocial.constants.ValidationConstants.STRONG_PASSWORD_REGEX;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record UpdatePasswordRequest(
@@ -15,14 +17,11 @@ public record UpdatePasswordRequest(
 
         @NotBlank(message = "New Password is required")
         @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters long")
+        @Pattern(regexp = STRONG_PASSWORD_REGEX,message = "New Password must have at least 1 number, 1 special character and 1 uppercase and lowercase character")
         String newPassword
 ) {
     @AssertTrue(message = "New Password must be different from Old Password")
     private boolean isNewPasswordDifferent() {
         return !newPassword.equals(oldPassword);
-    }
-    @AssertTrue(message = "New Password must have at least 1 number, 1 special character and 1 uppercase and lowercase character")
-    private boolean isNewPasswordStrong() {
-        return Validations.isPasswordStrong(newPassword);
     }
 }
--- src/main/java/com/joshtechnologygroup/minisocial/dto/user/UserCreateRequest.java
+++ src/main/java/com/joshtechnologygroup/minisocial/dto/user/UserCreateRequest.java
@@ -3,10 +3,11 @@ package com.joshtechnologygroup.minisocial.dto.user;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.joshtechnologygroup.minisocial.dto.userDetail.UserDetailCreateRequest;
-import com.joshtechnologygroup.minisocial.util.Validations;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
+
+import static com.joshtechnologygroup.minisocial.constants.ValidationConstants.STRONG_PASSWORD_REGEX;
 
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
@@ -18,6 +19,7 @@ public record UserCreateRequest(
 
         @NotBlank(message = "Password is required")
         @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters long")
+        @Pattern(regexp = STRONG_PASSWORD_REGEX, message = "Password must contain 1 Uppercase and Lowercase Letter, 1 number and 1 special character")
         String password,
 
         @NotNull(message = "active is required")
@@ -26,8 +28,4 @@ public record UserCreateRequest(
         @NotNull(message = "User Details are required")
         @Valid UserDetailCreateRequest userDetails
 ) {
-        @AssertTrue(message = "Password must contain 1 Uppercase and Lowercase Letter, 1 number and 1 special character")
-        private boolean isPasswordIsStrong() {
-                return Validations.isPasswordStrong(password);
-        }
 }
